99a267a46c3f54de469ea59dc0217fc0c07c01ae
minji105 <ajhjmj9@naver.com>
2024-08-05 00:16:10 +0900

    hashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Menu from './component/Buttons/Menu';
 import MainPage from './pages/MainPage';
@@ -10,13 +10,15 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <div className="App">
-      <Menu></Menu>
-      <Routes>
-        <Route path='/' element={<MainPage />} />
-        <Route path='/list' element={<List />} />
-        <Route path='/list/:category/:title' element={<Project />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
+      <Router>
+        <Menu></Menu>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/list' element={<List />} />
+          <Route path='/list/:category/:title' element={<Project />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
