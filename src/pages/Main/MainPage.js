5a4e80fc972c1b8cb84297c42331f36a2dcf42ee
minji105 <ajhjmj9@naver.com>
2024-10-30 18:35:59 +0900

    commit

--- src/pages/Main/MainPage.js
+++ src/pages/Main/MainPage.js
@@ -60,13 +60,16 @@ function MainPage() {
 
     let touchStartY = 0;
     let touchEndY = 0;
+
     const handleTouchStart = (e) => {
       touchStartY = e.changedTouches[0].screenY;
     };
+
     const handleTouchEnd = (e) => {
       touchEndY = e.changedTouches[0].screenY;
       handleGesture();
     };
+
     const handleGesture = () => {
       if (touchEndY < touchStartY && !animating) {
         gotoSection(currentIndex + 1, 1);
@@ -75,50 +78,13 @@ function MainPage() {
         gotoSection(currentIndex - 1, -1);
       }
     };
+    
     window.addEventListener("touchstart", handleTouchStart);
     window.addEventListener("touchend", handleTouchEnd);
-      window.addEventListener('wheel', handleWheel);
-
-    // const handleTouch = (e) => e.changedTouches[0].screenY;
-
-    // let removeEventListeners;
-
-    // const addEventListeners = () => {
-    //   window.addEventListener('wheel', handleWheel);
-
-    //   let touchStartY = 0;
-
-    //   const handleTouchStart = (e) => {
-    //     touchStartY = handleTouch(e);
-    //   };
-
-    //   const handleTouchEnd = (e) => {
-    //     const touchEndY = handleTouch(e);
-    //     if (!animating) {
-    //       const direction = touchEndY < touchStartY ? 1 : -1;
-    //       gotoSection(currentIndex + direction, direction);
-    //     }
-    //   };
-
-    //   window.addEventListener('touchstart', handleTouchStart);
-    //   window.addEventListener('touchend', handleTouchEnd);
-
-    //   removeEventListeners = () => {
-    //     window.removeEventListener('wheel', handleWheel);
-    //     window.removeEventListener('touchstart', handleTouchStart);
-    //     window.removeEventListener('touchend', handleTouchEnd);
-    //   };
-    // };
-
-    // const removeEventListeners = () => {
-    //   window.removeEventListener('wheel', handleWheel);
-    //   window.removeEventListener('touch', handleTouch);
-    // }
+    window.addEventListener('wheel', handleWheel);
 
     gotoSection(0, 1);
-    // addEventListeners();
 
-    // return () => removeEventListeners();
     return () => {
       window.removeEventListener("touchstart", handleTouchStart);
       window.removeEventListener("touchend", handleTouchEnd);
